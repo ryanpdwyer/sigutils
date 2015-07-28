baee62ec3e652b2d11dcadabd809194697cc91a6
Ryan Dwyer <ryanpdwyer@gmail.com>
2015-07-28 08:43:34 -0400

    Added fft function, test

--- sigutils/__init__.py
+++ sigutils/__init__.py
@@ -4,6 +4,8 @@
 sigutils
 ============================
 """
+import numpy as np
+from scipy import signal
 
 from sigutils.plot import (bode, bodes, bode_sys, bode_syss,
                            bode_z, bode_firs, bode_zz,
@@ -12,7 +14,45 @@ from sigutils.plot import (bode, bodes, bode_sys, bode_syss,
 
 from sigutils._util import log_bins, lin_bins, freqresp, freqz
 
+
+
+def fft(x, t=1, real=True, window='rect'):
+    """Compute the FFT of x using window. Return the FFT, and the
+    FFT frequencies.
+
+    x : array
+        The input data to Fourier transform
+    t : float or array, optional
+        Time information, used to adjust the Fourier transform frequencies.
+        If t is a number, use it as the time step. If t is an array,
+        infer the timestep from the array.
+    real : bool, optional
+        Compute only non-negative frequency coefficients, which contain all
+        the information if x is real.
+    window : str or array
+        Window to use; defaults to 'rect' (no window).
+    """
+    if isinstance(t, int) or isinstance(t, float):
+        dt = t
+    else:
+        dt = t[1] - t[0]
+
+    if real:
+        fft_func = np.fft.rfft
+        fft_freq_func = np.fft.rfftfreq
+    else:
+        fft_func = np.fft.fft
+        fft_freq_func = np.fft.fftfreq
+
+    window_factor = signal.get_window(window, x.size)
+
+    ft = fft_func(x * window_factor)
+    ft_freq = fft_freq_func(x.size, d=dt)
+
+    return ft, ft_freq
+
 # Versioneer versioning
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
--- sigutils/tests/test__init__.py
+++ sigutils/tests/test__init__.py
@@ -1,4 +1,16 @@
-import sigutils
+# -*- coding: utf-8 -*-
+from __future__ import division, absolute_import, print_function
+import unittest
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from sigutils import fft
 
-def test_import():
-    pass
+
+class TestFFT(unittest.TestCase):
+    def test_fft_exact_bin(self):
+        x = np.array([0, 1, 0, -1, 0, 1, 0, -1])
+        ft, ft_freq = fft(x, t=1, real=True, window='rect')
+        ft_expected = np.array([0, 0, -4j, 0, 0])
+        ft_freq_expected = np.arange(5) / 8
+        assert_array_almost_equal(ft, ft_expected)
+        assert_array_almost_equal(ft_freq, ft_freq_expected)
